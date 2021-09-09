b0cab200d5ec9472812357239e9127522adee331
pauloons <pauloons@gmail.com>
2021-09-09 00:35:23 -0300

    primeiro commit

---
+++ src/test/java/br/com/alura/spring/bootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.bootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
